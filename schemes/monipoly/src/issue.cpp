9197e7c2f36fa9876b78168eb68d7e0c976c2619
adttilove <1963096826@qq.com>
2026-08-18 14:04:22 +0800

    refactor(monipoly): simplify issuance to direct signing
    - replace split s1 and s2 randomness with a single s
    - remove the blind-issuance proof and redundant validity checks
    - avoid materializing parsed public-key elements

--- schemes/monipoly/src/issue.cpp
+++ schemes/monipoly/src/issue.cpp
@@ -1,5 +1,3 @@
-#include <stdexcept>
-
 #include <crypto12381/crypto12381.hpp>
 #include <monipoly.hpp>
 
@@ -44,38 +42,15 @@ namespace anonymous_credentials
         auto [b, c, u, tiled_g, tiled_X] = parse<G1^3|G2^2>(pk.fixed_part);
         auto Z = parse<G1>(upk);
         auto a = parse<Zp>(attr);
-        auto A = parse<G1>(pk.a) | materialize;
+        auto A = parse<G1>(pk.a);
         auto o = random-select_in<*Zp>;
 
         auto k = encode(a, o);
 
         auto C = Π[k.size()](A[i]^k[i]);
-        auto s1 = random-select_in<*Zp>;
-        auto M = C * Z * (b^s1);
-
-        auto m_tilde = random-select_in<Zp>(k.size()) | materialize;
-        auto s1_tilde = random-select_in<*Zp>;
-        auto R = Π[k.size()](A[i]^m_tilde[i]) * (b^s1_tilde);
-
-        auto e = hash(pk.fixed_part, pk.a, pk.tilde_a, upk, M, R).to(Zp);
-        auto m_hat = (m_tilde[i] + e * k[i]) (i.in[k.size()]);
-        auto s1_hat = (s1_tilde + e * s1);
-
-        auto L = Π[k.size()](A[i]^m_hat[i]) * (b^s1_hat);
-        if(L != R * ((M / Z)^e))
-        {
-            throw std::runtime_error{ "MoniPoly blind issuing initialization proof failed" };
-        }
-
         auto t = random-select_in<*Zp>;
-        auto s2 = random-select_in<*Zp>;
-        auto v = (M * (b^s2) * c)^inverse(x + t);
-        auto s = s1 + s2;
-
-        if(pair(v, tiled_X * (tiled_g^t)) != pair(C * Z * (b^s) * c, tiled_g))
-        {
-            throw std::runtime_error{ "MoniPoly issuer returned an invalid SDH-CL signature" };
-        }
+        auto s = random-select_in<*Zp>;
+        auto v = (C * Z * (b^s) * c)^inverse(x + t);
 
         return serialize(v, t, s, o);
     }
